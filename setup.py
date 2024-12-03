ad620b73e002a0f2454a075e766db915dfb63308
paras-dahiya <dparas.pd@gmail.com>
2024-12-03 21:27:49 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen ai project',
+    version= '0.0.0',
+    author= 'Paras Dahiya',
+    author_email= 'dparas.pd@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
